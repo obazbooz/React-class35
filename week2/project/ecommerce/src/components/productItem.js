15e22d5e91489417d2a1e10279daee6849162102
Osama Bazbooz <obazbooz@gmail.com>
2022-04-30 15:17:01 +0200

    Dublication created for learning porpuses as been deleted

--- week2/project/ecommerce/src/components/productItem.js
+++ week2/project/ecommerce/src/components/productItem.js
@@ -4,21 +4,12 @@ import ProductItemDetails from './productItemDetails.js';
 
 function ProductsItem({ product: { id, title, description, image } }) {
   return (
-    (
-      <Router>
-        <Routes>
-          <Route path="/product/:id" element={<ProductItemDetails />} />
-        </Routes>
-      </Router>
-    ),
-    (
-      <li className="productContainer">
-        <Link to={`/product/${id}`}>
-          <img className="productImg" src={image} alt={title} />
-          <p className="productDesc">{description}</p>
-        </Link>
-      </li>
-    )
+    <li className="productContainer">
+      <Link to={`/product/${id}`}>
+        <img className="productImg" src={image} alt={title} />
+        <p className="productDesc">{description}</p>
+      </Link>
+    </li>
   );
 }
 
